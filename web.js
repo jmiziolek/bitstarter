a8e3e134b8f5f9202d75dc8493e4d2366ca658ba
Jakub Miziołek <mail@jakubmiziolek.pl>
2013-07-06 12:57:28 +0000

    Message from index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
+
+var buff = fs.readFileSync("index.html");
+var index = new Buffer(buff);
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(index.toString());
 });
 
 var port = process.env.PORT || 5000;
